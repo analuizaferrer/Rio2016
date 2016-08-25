ea82ae6313333acb94001f415a4efa1f2e06204a
Ana Luiza Ferrer <analcferrer@gmail.com>
2016-08-25 15:39:11 -0300

    Separated StickerViewController into two ViewControllers.

---
+++ Rio2016/NewStickerViewController.swift
@@ -0,0 +1,186 @@
+//
+//  NewStickerViewController.swift
+//  Rio2016
+//
+//  Created by Ana Luiza Ferrer on 8/25/16.
+//  Copyright © 2016 Ana Luiza Ferrer. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+import AVFoundation
+
+class NewStickerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //CoreData
+    var stickerManagedObject: NSManagedObject!
+    
+    //AVFoundation
+    var session: AVCaptureSession!
+    var input: AVCaptureDeviceInput!
+    var output: AVCaptureStillImageOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    var selectedDevice: String?
+    var photo: UIImage?
+    
+    var confirmationView: UIView!
+    
+    let photoLibraryButton = UIButton(frame: CGRectMake(-20,600,320,50))
+    let cameraButton = UIButton(frame: CGRectMake(170,600,320,50))
+    
+    //ConfirmationView labels and buttons
+    var pictureImageView = UIImageView(frame: CGRectMake(0, 0, 375, 590))
+    let confirmButton = UIButton(frame: CGRectMake(170,600,320,50))
+    let cancelButton = UIButton(frame: CGRectMake(-20,600,320,50))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //AVFoundation
+        setupSession()
+
+        self.view.backgroundColor = UIColor.cyanColor()
+        
+        photoLibraryButton.setTitle(NSLocalizedString("PHOTO_LIBRARY_BUTTON", comment: ""), forState: UIControlState.Normal)
+        photoLibraryButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        photoLibraryButton.titleLabel?.textAlignment = NSTextAlignment.Left
+        photoLibraryButton.addTarget(self, action: #selector(NewStickerViewController.photoLIbraryButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(photoLibraryButton)
+        
+        cameraButton.setTitle(NSLocalizedString("CAMERA_BUTTON", comment: ""), forState: UIControlState.Normal)
+        cameraButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        cameraButton.titleLabel?.textAlignment = NSTextAlignment.Left
+        cameraButton.addTarget(self, action: #selector(NewStickerViewController.cameraButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(cameraButton)
+        
+        //ConfirmationView labels and buttons
+        confirmationView = UIView(frame: CGRectMake(0, 0, view.frame.width, view.frame.height))
+        confirmationView.backgroundColor = UIColor.purpleColor()
+        
+        confirmationView.addSubview(pictureImageView)
+        
+        confirmButton.setTitle(NSLocalizedString("CONFIRM_BUTTON", comment: ""), forState: UIControlState.Normal)
+        confirmButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        confirmButton.titleLabel?.textAlignment = NSTextAlignment.Left
+        confirmButton.addTarget(self, action: #selector(NewStickerViewController.confirmButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
+        confirmationView.addSubview(confirmButton)
+        
+        cancelButton.setTitle(NSLocalizedString("CANCEL_BUTTON", comment: ""), forState: UIControlState.Normal)
+        cancelButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        cancelButton.titleLabel?.textAlignment = NSTextAlignment.Left
+        cancelButton.addTarget(self, action: #selector(NewStickerViewController.cancelButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
+        confirmationView.addSubview(cancelButton)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupSession() {
+        
+        session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let camera = AVCaptureDevice
+            .defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        do { input = try AVCaptureDeviceInput(device: camera) } catch { return }
+        
+        output = AVCaptureStillImageOutput()
+        output.outputSettings = [ AVVideoCodecKey: AVVideoCodecJPEG ]
+        
+        guard session.canAddInput(input)
+            && session.canAddOutput(output) else { return }
+        
+        session.addInput(input)
+        session.addOutput(output)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        
+        previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+        previewLayer!.connection?.videoOrientation = .Portrait
+        
+        self.view.layer.addSublayer(previewLayer!)
+        
+        session.startRunning()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        previewLayer?.frame = CGRectMake(0, 64, view.frame.width, 455)
+    }
+
+    
+    func photoLIbraryButtonAction() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        photo = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
+        
+        self.pictureImageView.image = photo
+        
+        self.selectedDevice = "photo library"
+        self.view.addSubview(self.confirmationView)
+        
+    }
+    
+    func cameraButtonAction () {
+        
+        guard let connection = output.connectionWithMediaType(AVMediaTypeVideo) else { return }
+        connection.videoOrientation = .Portrait
+        
+        output.captureStillImageAsynchronouslyFromConnection(connection) { (sampleBuffer, error) in
+            guard sampleBuffer != nil && error == nil else { return }
+            
+            let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+            guard let image = UIImage(data: imageData) else { return }
+            
+            self.pictureImageView.image = image
+            self.photo = image
+            
+            self.selectedDevice = "camera"
+            self.view.addSubview(self.confirmationView)
+            
+        }
+    }
+    
+    func confirmButtonAction () {
+        
+        if self.selectedDevice == "camera" {
+           
+            UIImageWriteToSavedPhotosAlbum(photo!, nil, nil, nil)
+        
+        }
+        
+        base64Encode(photo!)
+        
+        self.navigationController?.popViewControllerAnimated(true)
+        
+    }
+    
+    func base64Encode (image: UIImage) {
+        
+        let imageData: NSData = UIImagePNGRepresentation(image)!
+        let strBase64: String = imageData.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+        
+    }
+    
+    func cancelButtonAction () {
+        
+        self.confirmationView.removeFromSuperview()
+        
+    }
+
+}
--- Rio2016/StickerViewController.swift
+++ Rio2016/StickerViewController.swift
@@ -12,49 +12,36 @@ import CoreLocation
 import CoreData
 import AVFoundation
 
-class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    var pictureView: UIView!
-    var confirmationView: UIView!
+class StickerViewController: UIViewController, CLLocationManagerDelegate {
     
     //CoreLocation
     var locationManager = CLLocationManager()
     var location = CLLocation()
     
-    //AVFoundation
-    var session: AVCaptureSession!
-    var input: AVCaptureDeviceInput!
-    var output: AVCaptureStillImageOutput!
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    
     //CoreData
     var stickerManagedObject: NSManagedObject!
     
-    //View labels and buttons
-    let nameLabel = UILabel(frame: CGRectMake(20,100,320,50))
-    let locationLabel = UILabel(frame: CGRectMake(20,200,320,50))
+    //Labels and buttons
+    let stickerImageView = UIImageView(frame: CGRectMake(0,100,400,350))
+    let nameLabel = UILabel(frame: CGRectMake(100,70,320,50))
     var getStickerButton = UIButton(frame: CGRectMake(100,500,320,50))
+    let locationStatusLabel = UILabel(frame: CGRectMake(20,500,320,50))
     var descriptionText = UITextView(frame: CGRectMake(100,500,320,50))
-    
-    //PictureView labels and buttons
-    let photoLibraryButton = UIButton(frame: CGRectMake(-20,600,320,50))
-    let cameraButton = UIButton(frame: CGRectMake(170,600,320,50))
-    var pictureImageView = UIImageView(frame: CGRectMake(0, 0, 375, 590))
-    
-    //ConfirmationView labels and buttons
-    let confirmButton = UIButton(frame: CGRectMake(170,600,320,50))
-    let cancelButton = UIButton(frame: CGRectMake(-20,600,320,50))
-    
-    var selectedDevice: String?
-    
-    var photo: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateLocation()
         
-    //View labels and buttons
+        //Labels and buttons setup
+        
+        if stickerManagedObject.valueForKey("image") as? String != "" {
+            
+            //display image here
+            
+        }
+        
+        self.view.addSubview(stickerImageView)
         
         nameLabel.text = stickerManagedObject.valueForKey("name") as? String
         self.view.addSubview(nameLabel)
@@ -62,70 +49,17 @@ class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImag
         descriptionText.text = stickerManagedObject.valueForKey("stickerDescription") as? String
         self.view.addSubview(descriptionText)
         
+        self.view.addSubview(locationStatusLabel)
+        
         getStickerButton.setTitle(NSLocalizedString("GET_STICKER_BUTTON", comment: ""), forState: UIControlState.Normal)
         getStickerButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
-        getStickerButton.addTarget(self, action: #selector(StickerViewController.pictureCaptureView), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        
-    //PictureView labels and buttons
-        pictureView = UIView(frame: CGRectMake(0, 0, view.frame.width, view.frame.height))
-        pictureView.backgroundColor = UIColor.cyanColor()
-        
-        photoLibraryButton.setTitle(NSLocalizedString("PHOTO_LIBRARY_BUTTON", comment: ""), forState: UIControlState.Normal)
-        photoLibraryButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-        photoLibraryButton.titleLabel?.textAlignment = NSTextAlignment.Left
-        photoLibraryButton.addTarget(self, action: #selector(StickerViewController.photoLIbraryButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
-        pictureView.addSubview(photoLibraryButton)
-        
-        cameraButton.setTitle(NSLocalizedString("CAMERA_BUTTON", comment: ""), forState: UIControlState.Normal)
-        cameraButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-        cameraButton.titleLabel?.textAlignment = NSTextAlignment.Left
-        cameraButton.addTarget(self, action: #selector(StickerViewController.cameraButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
-        pictureView.addSubview(cameraButton)
-        
-    //ConfirmationView labels and buttons
-        confirmationView = UIView(frame: CGRectMake(0, 0, view.frame.width, view.frame.height))
-        confirmationView.backgroundColor = UIColor.purpleColor()
-        
-        confirmButton.setTitle(NSLocalizedString("CONFIRM_BUTTON", comment: ""), forState: UIControlState.Normal)
-        confirmButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-        confirmButton.titleLabel?.textAlignment = NSTextAlignment.Left
-        confirmButton.addTarget(self, action: #selector(StickerViewController.confirmButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
-        confirmationView.addSubview(confirmButton)
-        
-        cancelButton.setTitle(NSLocalizedString("CANCEL_BUTTON", comment: ""), forState: UIControlState.Normal)
-        cancelButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-        cancelButton.titleLabel?.textAlignment = NSTextAlignment.Left
-        cancelButton.addTarget(self, action: #selector(StickerViewController.cancelButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
-        confirmationView.addSubview(cancelButton)
-        
-        confirmationView.addSubview(pictureImageView)
-        
-    //AVFoundation
-        
-        setupSession()
+        getStickerButton.addTarget(self, action: #selector(StickerViewController.getStickerButtonAction), forControlEvents: UIControlEvents.TouchUpInside)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func locationManager(manager: CLLocationManager,
-                         didChangeAuthorizationStatus status: CLAuthorizationStatus)
-    {
-        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
-            manager.startUpdatingLocation()
-        }
-    }
-    
-    func updateLocation() {
-        
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        checkCoreLocationPermission()
     }
     
     func checkCoreLocationPermission () {
@@ -145,10 +79,20 @@ class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImag
         }
     }
     
-    func showAlert() {
-        let alert = UIAlertController(title: NSLocalizedString("ALERT_TITLE", comment: ""), message: NSLocalizedString("ALERT_BODY", comment: ""), preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
+    func locationManager(manager: CLLocationManager,
+                         didChangeAuthorizationStatus status: CLAuthorizationStatus)
+    {
+        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    func updateLocation() {
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        checkCoreLocationPermission()
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -159,6 +103,12 @@ class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImag
         compareLocation()
     }
     
+    func showAlert() {
+        let alert = UIAlertController(title: NSLocalizedString("ALERT_TITLE", comment: ""), message: NSLocalizedString("ALERT_BODY", comment: ""), preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     func compareLocation () {
         
         let stickerLat = stickerManagedObject.valueForKey("latitude") as! Double
@@ -167,100 +117,21 @@ class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImag
         
         if location.distanceFromLocation(stickerLocation) < 500 {
             
-            locationLabel.text = NSLocalizedString("HERE_LABEL", comment: "")
+            locationStatusLabel.text = NSLocalizedString("HERE_LABEL", comment: "")
             self.view.addSubview(getStickerButton)
 
         }
         
         else {
             
-            locationLabel.text = NSLocalizedString("VISIT", comment: "") + " " + String(stickerManagedObject.valueForKey("name")!) + " " + NSLocalizedString("TO_GET_STICKER", comment: "")
+            locationStatusLabel.text = NSLocalizedString("VISIT", comment: "") + " " + String(stickerManagedObject.valueForKey("name")!) + " " + NSLocalizedString("TO_GET_STICKER", comment: "")
             
         }
         
-        self.view.addSubview(locationLabel)
-        
-    }
-    
-    func cameraButtonAction () {
-       
-        guard let connection = output.connectionWithMediaType(AVMediaTypeVideo) else { return }
-        connection.videoOrientation = .Portrait
-        
-        output.captureStillImageAsynchronouslyFromConnection(connection) { (sampleBuffer, error) in
-            guard sampleBuffer != nil && error == nil else { return }
-            
-            let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-            guard let image = UIImage(data: imageData) else { return }
-            
-            self.pictureImageView.image = image
-//            self.confirmationView.addSubview(self.pictureImageView)
-            
-            self.photo = image
-            
-            self.view.addSubview(self.confirmationView)
-            
-            self.selectedDevice = "camera"
-        }
-    }
-
-    func photoLIbraryButtonAction() {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .PhotoLibrary
-        presentViewController(picker, animated: true, completion: nil)
-        
-        self.view.addSubview(confirmationView)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        
-        photo = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
-        
-        self.pictureImageView.image = photo
-        self.pictureView.addSubview(self.pictureImageView)
-        
-        self.selectedDevice = "photo library"
-        
-    }
-    
-    func setupSession() {
-       
-        session = AVCaptureSession()
-        session.sessionPreset = AVCaptureSessionPresetPhoto
-        
-        let camera = AVCaptureDevice
-            .defaultDeviceWithMediaType(AVMediaTypeVideo)
-        
-        do { input = try AVCaptureDeviceInput(device: camera) } catch { return }
-        
-        output = AVCaptureStillImageOutput()
-        output.outputSettings = [ AVVideoCodecKey: AVVideoCodecJPEG ]
-        
-        guard session.canAddInput(input)
-            && session.canAddOutput(output) else { return }
-        
-        session.addInput(input)
-        session.addOutput(output)
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        
-        previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
-        previewLayer!.connection?.videoOrientation = .Portrait
-        
-        pictureView.layer.addSublayer(previewLayer!)
-        
-        session.startRunning()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        previewLayer?.frame = CGRectMake(0, 64, view.frame.width, 455)
     }
     
     func presentActivityVCForImage(image: UIImage) {
@@ -271,26 +142,23 @@ class StickerViewController: UIViewController, CLLocationManagerDelegate, UIImag
         )
     }
     
-    func pictureCaptureView () {
+    func getStickerButtonAction () {
         
-        self.view.addSubview(pictureView)
+        performSegueWithIdentifier("segueToNewSticker", sender: self)
         
     }
     
-    func confirmButtonAction () {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
-        if self.selectedDevice == "camera" {
-            UIImageWriteToSavedPhotosAlbum(photo!, nil, nil, nil)
+        if segue.identifier == "segueToNewSticker" {
+            
+            let nextVC =  segue.destinationViewController as! NewStickerViewController
+            
+            nextVC.stickerManagedObject = stickerManagedObject
+            
         }
         
-//        self.presentViewController(AlbumCollectionViewController(), animated: true, completion: nil)
-        
     }
-    
-    func cancelButtonAction () {
-        
-        self.view = pictureView
-        
-    }
+
 
 }
